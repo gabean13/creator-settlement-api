54442fcd9d8d4bee9f9b04f67ea3dd985d680b6e
Gabin Choi <choegabin171@gmail.com>
2026-04-26 21:21:53 +0900

    docs: 프로젝트 초기 설정

---
+++ src/test/java/com/liveclass/creatorsettlement/CreatorSettlementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liveclass.creatorsettlement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreatorSettlementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
